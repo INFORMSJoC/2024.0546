58cf09038bccae8146ad74f85e5d73b9d3649afc
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-11 14:27:52 +0800

    Update db.m

--- src/Comp/ALogTP/db.m
+++ src/Comp/ALogTP/db.m
@@ -1,7 +1,7 @@
 function y = db(x)
-%  db: the deriative of operator b
-%  Input x: a real number larger than -1
-%  output: 1/(x+1)
+%  db: the derivative of operator b.
+%  Input x: a real number larger than -1.
+%  Output: 1/(x+1).
     y= 1/(x+1);
 end
 
