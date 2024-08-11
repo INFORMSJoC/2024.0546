fa44d497ad83839ce4f1864941d6858ed299fec8
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-11 14:34:11 +0800

    Update insert.m

--- src/Comp/DaE/insert.m
+++ src/Comp/DaE/insert.m
@@ -1,5 +1,5 @@
 function y = insert(net,Link)
-% insert: combine the nonrobust links with the robust ones
+% insert: to combine the nonrobust links with the robust ones.
 % Input: a num*1 vector net, recording the strengths of the non-robust
 % links in a subnetwork; Link in [0,1]^L, a vector that records the structure of the subnetwork.
 % Output: a network in [0,1]^L derived from Link by replacing
