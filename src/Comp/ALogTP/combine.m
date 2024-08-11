aba56625ee58eaa08afd9d88171eaf811bad4d3f
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-11 14:27:38 +0800

    Update combine.m

--- src/Comp/ALogTP/combine.m
+++ src/Comp/ALogTP/combine.m
@@ -1,9 +1,9 @@
 function tilde_D = combine(M,D)
-% combine: to figure out the nonrobust links
+% combine: to figure out the nonrobust links.
 % Input: two N*N matrice M and D derived from "robust link.m".
-% Output: a N × N matrix ˜D. For each link ij, ˜Dij = 0 if the link is surely to be absent
+% Output: a N*N matrix ˜D. For each link ij, ˜Dij = 0 if the link is surely to be absent
 % or built and ˜Dij = 1 otherwise. With this matrix, we can decompose the network
-% into smaller ones (via the Matlab function “conncomp(˜D )”).
+% into smaller ones. (via the Matlab function “conncomp(˜D )”).
 global N
     tilde_D = zeros(N,N);
     for i = 1:N
