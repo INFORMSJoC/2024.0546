5d61f8b159fcbc273744b026f81005c91c91654e
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-11 15:44:38 +0800

    Update loop_graph.m

--- src/LogTPm/Version:connections/loop_graph.m
+++ src/LogTPm/Version:connections/loop_graph.m
@@ -1,7 +1,7 @@
 function z = loop_graph(L)
 % loop_graph: all possible networks when assuming the agents
 % are located on a circle.
-% Input L: the number of links.
+% Input: L, the number of links.
 % Output: a 2^L*L matrix whose each row represents a possible network.
 
 
