75fe58b270faae2d91e61fb43545609fcf8797ac
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-11 11:49:08 +0800

    Update search_subproblem.m

--- src/Comp/DaE/search_subproblem.m
+++ src/Comp/DaE/search_subproblem.m
@@ -1,9 +1,7 @@
 function group = search_subproblem(N,S,D,num_s)
-% search_subproblem: to modify the results of the Matlab function “conncomp”
-% in the decomposition step.
-% Input: N, number of players; S, the output of "conncomp(˜D)"; D, the N × N matrix derived from "robust
-% link.m”; num S, the number of connected conponents computed by Matlab
-% codes max(S).
+% search_subproblem: to modify the results of the Matlab function “conncomp” in the decomposition step.
+% Input: N, number of players; S, the output of "conncomp(˜D)"; D, the N × N matrix derived from "robust_
+% link.m”; num S, the number of connected conponents computed by Matlab codes max(S).
 % Output: group, a matrix of N columns. Each row of the matrix corresponds to
 % a subnetwork. Its (i, j)-th element equals to 1 if agent j is included in the i-th
 % subnetwork and 0 otherwise.
