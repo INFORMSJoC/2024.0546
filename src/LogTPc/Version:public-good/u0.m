e2e3929ca46797a98cd77743d54157a5d94764a5
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-11 14:19:06 +0800

    Update u0.m

--- src/LogTPc/Version:public-good/u0.m
+++ src/LogTPc/Version:public-good/u0.m
@@ -1,7 +1,7 @@
 function z = u0(x,i,j)
-%  u0: to compute the elements of the homotopy system at t=0
-%  Input: x: link strength; i in {1,2..L},j in {1,2}, index for the link and player (in the sense of "lin");
-%  output: the 2*i+2-j -th element of the the homotopy system at t=0
+%  u0: to compute the elements of the homotopy system at t=0.
+%  Input: x, link strength; i in {1,2..L},j in {1,2}, index for the link and player (in the sense of "lin").
+%  Output: the 2*i+2-j -th element of the the homotopy system at t=0.
 global pri d e N lin L Sigma eta
     player = lin(i,j);
     for k = 1:L
