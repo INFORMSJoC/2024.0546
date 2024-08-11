76e84bf522e56b8b34a44ec35e32335fb550ccc0
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-11 14:01:37 +0800

    Update homof.m

--- src/LogTPm/Version:connections/homof.m
+++ src/LogTPm/Version:connections/homof.m
@@ -1,7 +1,7 @@
 function f = homof(x)
 % homof: to compute the homotopy system.
 % Input: x, a point in [0,1]^{2L+1}.
-% output: y, the value of the homotopy system (in [0,1]^{2L}).
+% Output: y, the value of the homotopy system (in [0,1]^{2L}).
 global L M Sigma lin pri eta p
 
 t = x(M+1);
