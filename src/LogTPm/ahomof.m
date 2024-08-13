b3955a5f2877dc041414c44a88a8710f20509016
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-13 21:43:08 +0800

    Update ahomof.m

--- src/LogTPm/ahomof.m
+++ src/LogTPm/ahomof.m
@@ -1,5 +1,5 @@
 function f = ahomof(x)
-% ahomof: the system of equations in the corrector step.
+% ahomof: the system of equations to solve in the corrector step.
 % Input: x, a point in [0,1]^{2L+1}.
 % Output: y, the value of the system of equations in the corrector step (in [0,1]^{2L+1}).
 global d0 b0
