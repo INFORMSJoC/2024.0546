3e380a532ada625b12622a4301f3c026624d3061
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-13 21:56:20 +0800

    Update link.m

--- src/LogTPc/link.m
+++ src/LogTPc/link.m
@@ -3,8 +3,8 @@ function z = link(numP)
 % Input numP: number of players. 
 % Output: a L*2 matrix whose each row represents a possible link. For
 % example, the row given by (i,j) represents the link between agent i and j.  
-numL = numP*(numP-1)/2;% number of links
-z = zeros(numL,2);
+L = numP*(numP-1)/2;% number of links
+z = zeros(L,2);
 k = 1;
 for i = 1:numP
     z(k:k+numP-i-1,1) = i*ones(numP-i,1);
