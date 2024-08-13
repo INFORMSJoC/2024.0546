499b9e7d1ccf8f329a017658d65bd183a28a36e4
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-13 21:57:41 +0800

    Update u0.m

--- src/LogTPc/Version:public-good/u0.m
+++ src/LogTPc/Version:public-good/u0.m
@@ -1,5 +1,5 @@
 function z = u0(x,i,j)
-%  u0: to compute the elements of the homotopy system at t=0.
+%  u0: to compute the elements of the homotopy system at t=0. (for the public good provision model)
 %  Input: x, link strength; i in {1,2..L},j in {1,2}, index for the link and player (in the sense of "lin").
 %  Output: the 2*i+2-j -th element of the the homotopy system at t=0.
 global pri d e N lin L Sigma eta
