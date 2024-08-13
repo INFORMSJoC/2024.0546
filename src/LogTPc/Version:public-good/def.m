a5513aae16d7f7304b7b7b1a018772e29b5c3e2d
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-13 21:57:29 +0800

    Update def.m

--- src/LogTPc/Version:public-good/def.m
+++ src/LogTPc/Version:public-good/def.m
@@ -1,6 +1,6 @@
 function z = def(s,x)
 % def: to compute the partial derivatives of the utility functions with respect to
-% a given network.
+% a given network. (for the public good provision model)
 % Input: s in [0,1]^{2L}, the vector of favorite strengths; x in [0,1]^L, a given network.
 % Output: a N*N whose (i,j) and (j,i)-th element represents the partial derivatives
 % of agent i's and j's utility function with respect to link ij, respectively.
