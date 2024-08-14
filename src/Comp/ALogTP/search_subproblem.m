7f041a42a2c1877d5e4c66c9b9b48e98974556f6
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-14 13:56:35 +0800

    Update search_subproblem.m

--- src/Comp/ALogTP/search_subproblem.m
+++ src/Comp/ALogTP/search_subproblem.m
@@ -2,7 +2,7 @@ function group = search_subproblem(N,S,D,num_s)
 % search_subproblem: to figure out the agents involved in each subproblem. A subproblem contains the agents 
 % in a connected component of the graph given by ˜D and the agents connected to them via a robustly built link.
 % Input: N, number of players; S, the output of "conncomp(˜D)”; D, the N × N matrix derived from “robust
-% link.m”; num S, the number of connected conponents computed by Matlab
+% link.m”; num S, the number of connected components computed by Matlab
 % codes max(S).
 % Output: group, a matrix of N columns. Each row of the matrix corresponds to
 % a subnetwork. Its (i, j)-th element equals to 1 if agent j is included in the i-th
