399f4890c6889021ef3310232e5ca8e9059a6799
TTaoJH <145257619+TTaoJH@users.noreply.github.com>
2024-08-14 13:57:10 +0800

    Update search_subproblem.m

--- src/Comp/DaE/search_subproblem.m
+++ src/Comp/DaE/search_subproblem.m
@@ -1,7 +1,7 @@
 function group = search_subproblem(N,S,D,num_s)
 % search_subproblem: to figure out the agents involved in each subproblem.
 % Input: N, number of players; S, the output of "conncomp(˜D)"; D, the N × N matrix derived from "robust_
-% link.m”; num S, the number of connected conponents computed by Matlab codes max(S).
+% link.m”; num S, the number of connected components computed by Matlab codes max(S).
 % Output: group, a matrix of N columns. Each row of the matrix corresponds to
 % a subnetwork. Its (i, j)-th element equals to 1 if agent j is included in the i-th
 % subnetwork and 0 otherwise.
